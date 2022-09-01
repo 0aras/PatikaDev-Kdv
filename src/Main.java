d5c2a984893f2d8ae99c921048ecfc9b5a071156
0aras <mailaras00@gmaill.com>
2022-09-01 21:05:23 +0300

    result

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,15 @@ public class Main {
         Scanner tara=new Scanner(System.in);
         System.out.println("Lütfen ürün fiyatını giriniz");
         float price=tara.nextInt();
+
         if(price>0&&price<=1000){
-            System.out.println("KDV'li fiyat: "+(price*1.18)+"\nKDV oranı %18");
+            float a= (float) (price*1.18);
+            float d=a-price;
+            System.out.println("KDV'li fiyat: "+a+"\nKDV oranı %18"+"\nKDV fiyat farkı: "+d+" TL");
         }else if(price>1000){
-            System.out.println("KDV'li fiyat: "+(price*1.08)+"\nKDV oranı %08");
+            float b= (float) (price*1.08);
+            float c=b-price;
+            System.out.println("KDV'li fiyat: "+b+"\nKDV oranı %08"+ "\nKDV fiyat farkı: "+c+" TL");
         }else{
             System.out.println("Yanlış bir değer girdiniz");
         }
